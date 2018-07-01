bb5430c5ca898bd62f1df756290de049a185075c
Matiboux <matiboux@gmail.com>
2018-07-01 02:52:43 +0200

    Updated run script

--- run.bat
+++ run.bat
@@ -1,27 +1,21 @@
 @echo off
 cd %~dp0
-setlocal enabledelayedexpansion
+REM setlocal enabledelayedexpansion
 
 if not exist php\php.exe (
-	echo PHP wasn't detected; we'll download and install it for you.
+	echo PHP wasn't detected. Let's download and install now.
 	PowerShell -ExecutionPolicy Unrestricted -File "downloadphp.ps1"
+	echo.
 )
 
+:installphp
 if not exist php\php.exe (
-	echo Failed to setup php, try doing it manually
+	echo Failed to setup PHP, try doing it manually before continuing.
+	echo.
 	pause
-	exit
+	goto installphp
 )
-
-if not exist token.txt (
-	echo(
-	echo Go to https://steamcommunity.com/saliengame/gettoken and save that page as token.txt file
-	echo(
-	pause
-)
-
-echo The script can be terminated at any time by pressing Ctrl-C
 
 :start
-php\php.exe -f cheat.php
+php\php.exe -f mediadl.php
 goto start
